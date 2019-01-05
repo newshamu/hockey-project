91a40ac8184a4fb81d6091177640253dca204893
newshamu <johnsonc.2423@gmail.com>
2019-01-05 04:27:57 +0000

    Added some basic team info to the Team View

--- src/components/TeamView.vue
+++ src/components/TeamView.vue
@@ -1,7 +1,12 @@
 <template>
   <v-flex xs12 sm9>
     <v-card v-if="team !== null">
-      <v-card-title>{{ team.name }}</v-card-title>
+      <v-card-title>{{ team.name }} ({{ team.abbreviation }})</v-card-title>
+      <v-card-text>
+        <p><u>Division:</u> {{ team.division.name }}</p>
+        <p><u>Conference:</u> {{ team.conference.name }}</p>
+        <p><u>First year of play:</u> {{ team.firstYearOfPlay }}</p>
+      </v-card-text>
     </v-card>
   </v-flex>
 </template>
