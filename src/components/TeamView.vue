eb6d3688ebe9b4eec429576103cb095f2453daf4
newshamu <johnsonc.2423@gmail.com>
2019-01-07 03:30:30 +0000

    Deeper team stats fetched from API

--- src/components/TeamView.vue
+++ src/components/TeamView.vue
@@ -6,6 +6,7 @@
         <v-select
           :items="teams"
           v-model="team"
+          @change="getTeamStats()"
           outline
           item-text="name"
           label="Team"
@@ -17,16 +18,39 @@
         <p><u>Conference:</u> {{ team.conference.name }}</p>
         <p><u>First year of play:</u> {{ team.firstYearOfPlay }}</p>
       </v-card-text>
+      <v-card v-if="teamStats !== null">
+        <v-card-text>
+          <p><u>Games played to date:</u> {{ teamStats[0].splits[0].stat.gamesPlayed }}</p>
+          <p><u>Record to date:</u> 
+            {{ teamStats[0].splits[0].stat.wins }}-
+            {{ teamStats[0].splits[0].stat.losses }}-
+            {{ teamStats[0].splits[0].stat.ot }}
+          </p>
+        </v-card-text>
+      </v-card>
     </v-card>
   </v-flex>
 </template>
 
 <script>
+var axios = require('axios')
+
 export default {
   props: ['teams'],
   data: function () {
     return {
-      team: null
+      team: null,
+      teamStats: null
+    }
+  },
+  methods: {
+    getTeamStats: function () {
+      var that = this
+      var teamID = this.team.id
+      axios.get('https://statsapi.web.nhl.com/api/v1/teams/' + teamID + '?expand=team.stats')
+      .then( function (res) {
+        that.teamStats = res.data.teams[0].teamStats
+      })
     }
   }
 }
