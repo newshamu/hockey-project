17b2098db662087fd9b98ca8cff73c67ec0bdcca
newshamu <johnsonc.2423@gmail.com>
2019-01-23 00:10:42 +0000

    Charts drawn for each individual stat

--- src/components/StatChart.vue
+++ src/components/StatChart.vue
@@ -2,8 +2,10 @@
   <v-flex>
     <v-btn @click="updateChart">Create Chart</v-btn>
     <GChart
+      v-for="data in chartData"
+      :key="data.id"
       type="ColumnChart"
-      :data="chartData"
+      :data="data"
       :options="chartOptions"
     />
   </v-flex>
@@ -33,23 +35,54 @@ export default {
   methods: {
     updateChart: function () {
       // Initialize stat categories
+      // var chartData = []
+      // var statsText = []
+      // var statsValue = []
+      // for (var i = 0; i < this.selectedStats.length; i++) {
+      //   statsText.push(this.selectedStats[i].text)
+      //   statsValue.push(this.selectedStats[i].value)
+      // }
+      // chartData.push(statsText)
+
+      // // Populate data
+      // for (i = 0; i < this.selectedTeams.length; i++) {
+      //   var teamData = []
+      //   var currentTeam = this.selectedTeams[i]
+      //   for (var j = 0; j < statsText.length; j++) {
+      //     teamData.push(currentTeam[statsValue[j]])
+      //   }
+      //   chartData.push(teamData)
+      // }
+      // this.chartData = chartData
+
       var chartData = []
+      var stats = this.selectedStats
+
+      // Break stats into text and value arrays
       var statsText = []
       var statsValue = []
       for (var i = 0; i < this.selectedStats.length; i++) {
         statsText.push(this.selectedStats[i].text)
         statsValue.push(this.selectedStats[i].value)
       }
-      chartData.push(statsText)
 
-      // Populate data
-      for (i = 0; i < this.selectedTeams.length; i++) {
-        var teamData = []
-        var currentTeam = this.selectedTeams[i]
-        for (var j = 0; j < statsText.length; j++) {
-          teamData.push(currentTeam[statsValue[j]])
+      // Create a chart data array for each stat
+      // Loop starts at 1 to ignore team name "stat"
+      for (i = 1; i < this.selectedStats.length; i++) {
+        var data = []
+        var d = []
+        d.push(statsText[0])
+        d.push(statsText[i])
+        data.push(d)
+        d = []
+        // Insert each team's values for each stat
+        for (var j = 0; j < this.selectedTeams.length; j++) {
+          d.push(this.selectedTeams[j].name)
+          d.push(this.selectedTeams[j][stats[i].value])
+          data.push(d)
+          d = []
         }
-        chartData.push(teamData)
+        chartData.push(data)
       }
       this.chartData = chartData
     }
